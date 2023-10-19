9624e94f988289756cd557731230daf39e477af0
sgduran <sgduran91@gmail.com>
2023-10-19 15:46:22 +0200

    commit some changes

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ from helper_functions import task_solver
 begin_time = datetime.datetime.now()
 
 # Define global variables
-COMPANIES_LISTING_PATH = 'companies_listing.csv'
-MISSIONS_FOLDER = './missions/'
+COMPANIES_LISTING_PATH = './matching_problem/companies_listing.csv'
+MISSIONS_FOLDER = './matching_problem/missions/'
 SHARED_FREELANCES_FOR_TASK = 10
 
 
